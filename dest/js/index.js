13ef83c56527fd3215acf489ee32c58d432e90bd
SpiderQshka <39962817+SpiderQshka@users.noreply.github.com>
2019-06-18 17:28:14 +0300

    Add project to repository

---
+++ dest/js/index.js
@@ -0,0 +1,3 @@
+const slider = document.getElementById('slider'),
+	  sliderBtnRight = document.getElementById('slider-button-right'),
+	  sliderBtnLeft = document.getElementById('slider-button-left');
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const {src, dest, watch, series, task, parallel} = require('gulp'),
+	  sass = require('gulp-sass'),
+	  sync = require("browser-sync"),
+	  imagemin = require('imagemin'),
+	  prefixer = require('gulp-autoprefixer'),
+	  browserSync = require('browser-sync').create();
+
+var path = {
+	src: {
+		html: 'src/html/**/*.html',
+		style: 'src/styles/**/*.scss',
+		js: 'src/js/**/*.js',
+		image: 'src/img/**/*.*',
+		fonts: 'src/fonts/*.*'
+	},
+	dest: {
+		html: 'dest/html/',
+		style: 'dest/styles/',
+		js: 'dest/js/',
+		image: 'dest/img/',
+		fonts: 'dest/fonts/'
+	}
+};
+
+task('webserver', function(){
+	browserSync.init({
+	    server: {
+	        baseDir: "./dest",
+	       	index: './html/index.html'
+	    },
+	    tunnel: true,
+	    host: 'localhost',
+	    port: 3000
+	});
+})
+
+task('destHTML', function(){
+	return src(path.src.html)
+		.pipe(dest(path.dest.html))
+		.pipe(browserSync.stream());
+});
+
+task('destStyles', function(){
+	return src(path.src.style)
+		.pipe(sass())
+		.pipe(prefixer())
+		.pipe(dest(path.dest.style))
+		.pipe(browserSync.stream());
+});
+
+task('destScripts', function(){
+	return src(path.src.js)
+		.pipe(dest(path.dest.js))
+		.pipe(browserSync.stream());
+});
+
+task('destImages', function(){
+	return src(path.src.image)
+		.pipe(dest(path.dest.image))
+		.pipe(browserSync.stream());
+});
+
+task('destFonts', function(){
+	return src(path.src.fonts)
+		.pipe(dest(path.dest.fonts))
+		.pipe(browserSync.stream());
+});
+
+task('watchHTML', function(){
+	watch(path.src.html, series('destHTML'));
+});
+
+task('watchStyles', function(){
+	watch(path.src.style, series('destStyles'));
+});
+
+task('watchScripts', function(){
+	watch(path.src.js, series('destScripts'));
+});
+
+task('watchImages', function(){
+	watch(path.src.image, series('destImages'));
+});
+
+task('watchFonts', function(){
+	watch(path.src.fonts, series('destFonts'));
+});
+
+task('watchAll', parallel('watchHTML', 'watchStyles', 'watchScripts', 'watchImages', 'watchFonts'));
+
+task('default', parallel('webserver', 'watchAll'));
